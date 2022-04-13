752aa781440c271f4ae39433c42e36a2d75def5c
DenisKhamitsevich <den_02@tut.by>
2022-04-13 22:05:36 +0300

    added settings page and logic

--- src/main/java/com/market/ths/controllers/DefaultController.java
+++ src/main/java/com/market/ths/controllers/DefaultController.java
@@ -2,22 +2,57 @@ package com.market.ths.controllers;
 
 
 import com.market.ths.user.User;
+import com.market.ths.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
 
 @Controller
 @RequestMapping("/")
 public class DefaultController {
+    private final UserService userService;
+
+    @Autowired
+    public DefaultController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("account")
     public String getAccount(Model model){
-        User user = (User) SecurityContextHolder
+        User user = getLoggedUser();
+        user=userService.loadUserById(user.getId());
+        model.addAttribute("user",user);
+        return "account";}
+
+    @PostMapping("saveNameAndEmail")
+    public String saveNameAndEmail(@ModelAttribute("user") User user)
+    {
+        User loggedUser = getLoggedUser();
+        Long id=loggedUser.getId();
+        userService.UpdateNameAndEmail(id,user.getEmail(),user.getName());
+        return "redirect:account";
+    }
+
+    @PostMapping("savePassword")
+    public String savePassword(@RequestParam("password") String password)
+    {
+        User loggedUser=getLoggedUser();
+        Long id=loggedUser.getId();
+        userService.UpdatePassword(id,password);
+        return "redirect:account";
+    }
+
+    private User getLoggedUser()
+    {
+        User loggedUser = (User) SecurityContextHolder
                 .getContext()
                 .getAuthentication()
                 .getPrincipal();
-        model.addAttribute("user",user);
-        return "account";}
+        return loggedUser;
+    }
+
 }
--- src/main/java/com/market/ths/user/UserService.java
+++ src/main/java/com/market/ths/user/UserService.java
@@ -7,6 +7,9 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+import java.util.Optional;
+
 @Service
 public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
@@ -20,6 +23,11 @@ public class UserService implements UserDetailsService {
         return userRepository.findByEmail(username).orElseThrow(()->new UsernameNotFoundException("Email not found!"));
     }
 
+    public User loadUserById(Long id)
+    {
+        return userRepository.getById(id);
+    }
+
 
     public void SignUpUser(User user){
         boolean exists=userRepository.findByEmail(user.getEmail()).isPresent();
@@ -30,6 +38,26 @@ public class UserService implements UserDetailsService {
         BCryptPasswordEncoder encoder=new BCryptPasswordEncoder();
         String password=encoder.encode(user.getPassword());
         user.setPassword(password);
+        userRepository.save(user);
+    }
+
+    public void UpdateNameAndEmail(Long id, String newEmail, String name) {
+        User user = userRepository.getById(id);
+
+        if ((userRepository.findByEmail(newEmail).isPresent())&&(!Objects.equals(newEmail, user.getEmail()))) {
+            throw new RegistrationException("Пользователь с таким email уже существует!");
+        }
+        user.setEmail(newEmail);
+        user.setName(name);
+        userRepository.save(user);
+    }
+
+    public void UpdatePassword(Long id, String newPassword)
+    {
+        User user = userRepository.getById(id);
+        BCryptPasswordEncoder encoder=new BCryptPasswordEncoder();
+        String encodedPassword=encoder.encode(newPassword);
+        user.setPassword(encodedPassword);
         userRepository.save(user);
     }
 }
